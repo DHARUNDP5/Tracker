8bf4b294dca844f0e2c4e049c85b45dea7c38fcd
DHARUNDP5 <dharunprakashkdp@gmail.com>
2023-03-14 14:34:01 +0530

    Navbar created

--- tracker/src/Component/Navbar/Navbar.js
+++ tracker/src/Component/Navbar/Navbar.js
@@ -1,3 +1,16 @@
-<div>
-    header
-</div>
+
+import React from "react";
+import Logo from "../Images/Logo.svg";
+
+const Navbar = () => {
+  return (
+    <div className="  font-poppins  font-bold shadow-lg shadow-black-100  w-full h-0 flex-row flex items-center ">
+      <div>
+        <img className="pl-137" src={Logo} alt=''></img>
+      </div>
+      <span className="tracking-widest leading-3 pl-3 text-27 ">TRACKER</span>
+    </div>
+  );
+};
+
+export default Navbar;
