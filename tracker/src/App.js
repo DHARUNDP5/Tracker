552cc0d47fb41a24f3e38a2c8bb17f455065aebc
DHARUNDP5 <dharunprakashkdp@gmail.com>
2023-03-15 14:33:57 +0530

    Landing page finesed

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -1,12 +1,15 @@
-
+import { Routes, Route } from "react-router-dom";
 import Navbar from './Component/Navbar/Navbar.js';
 import Footer from './Component/Footer/Footer.js';
-
+import LandingPage from './Component/LandingPage/LandingPage.js';
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Footer/>
+          <Routes>
+      
+        <Route path="/" element={<LandingPage />} exact></Route>
+       
+             </Routes>
     </div>
   );
 }
